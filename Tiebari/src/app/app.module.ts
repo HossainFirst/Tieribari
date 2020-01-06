3cc23d6808e485d25d096e42fb6208c8018532c1
HossainFirst <49758415+HossainFirst@users.noreply.github.com>
2020-01-06 04:30:48 +0100

    Update app.module.ts

--- Tiebari/src/app/app.module.ts
+++ Tiebari/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { BLE } from '@ionic-native/ble/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Vibration } from '@ionic-native/vibration/ngx';
+import { BatteryStatus } from '@ionic-native/battery-status/ngx';
+//import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,9 +20,12 @@ import { AppComponent } from './app.component';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     BLE,
+    InAppBrowser,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Vibration,
+    BatteryStatus
   ],
   bootstrap: [AppComponent]
 })
